21e182b83450f01192c7f70f7b2b0e867efa8806
hanushchak <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>
2022-05-23 15:02:46 +0300

    create ShopListAdapter.kt  and fun setupRecyclerView

--- app/src/main/java/com/aureliano_vitalii/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/aureliano_vitalii/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -5,17 +5,19 @@ import androidx.lifecycle.MutableLiveData
 import com.aureliano_vitalii.shoppinglist.domain.ShopItem
 import com.aureliano_vitalii.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
+import kotlin.random.Random
+
 
 object ShopListRepositoryImpl: ShopListRepository {
 
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({o1, o2 -> o1.id.compareTo(o2.id)})
     private val shopListLD = MutableLiveData<List<ShopItem>>()
 
     private var autoIncrementId = 0
 
     init{
-        for(i in 0 until 10){
-            val item = ShopItem("Name $i", i ,true)
+        for(i in 0 until 1000){
+            val item = ShopItem("Name $i", i , Random.nextBoolean())
             addShopItem(item)
         }
     }
--- app/src/main/java/com/aureliano_vitalii/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/aureliano_vitalii/shoppinglist/presentation/MainActivity.kt
@@ -3,25 +3,46 @@ package com.aureliano_vitalii.shoppinglist.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.aureliano_vitalii.shoppinglist.R
+import com.aureliano_vitalii.shoppinglist.domain.ShopItem
+
 
 class MainActivity : AppCompatActivity() {
 
     private  lateinit var viewModel: MainViewModel
-    private var count =0
+    private lateinit var adapter: ShopListAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this){
-            Log.d("MainActivityTest", it.toString())
-            if(count == 0){
-                val item = it[0]
-                viewModel.changeShopItem(item)
-                count++
-            }
+            adapter.shopList = it
         }
     }
+
+    private fun setupRecyclerView(){
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
+        with(rvShopList) {
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_ENABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.VIEW_TYPE_DISABLED,
+                ShopListAdapter.MAX_POOL_SIZE
+            )
+        }
+    }
+
+
 }
