9880e11eebb806e756a3e53777f4811718e20f2b
hanushchak <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>
2022-05-26 11:31:13 +0300

    replaces the implementation of RecyclerView.Adapter on ListAdapter

---
+++ app/src/main/java/com/aureliano_vitalii/shoppinglist/presentation/ShopItemDiffCallback.kt
@@ -0,0 +1,15 @@
+package com.aureliano_vitalii.shoppinglist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.aureliano_vitalii.shoppinglist.domain.ShopItem
+
+class ShopItemDiffCallback: DiffUtil.ItemCallback<ShopItem>() {
+
+    override fun areItemsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+       return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: ShopItem, newItem: ShopItem): Boolean {
+        return oldItem == newItem
+    }
+}
